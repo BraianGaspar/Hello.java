a2624baa4de73f5499505b640247fa77af1bdab3
BRAIAN GASPAR DA ROSA <braian_gaspar@hotmail.com>
2025-02-26 21:39:17 -0300

    HelloWorld

---
+++ main.java
@@ -0,0 +1,15 @@
+public class main{
+    public static void main (String[] args){
+        HelloWorld();
+    }
+    public static HelloWorld(){
+        System.out.println("Hello world");
+    }
+    
+}
+
+
+
+
+
+
