5817a48d4c708b87858ec4e57b6c6b9bdc3bc4ef
BRAIAN GASPAR DA ROSA <braian_gaspar@hotmail.com>
2025-02-26 21:43:57 -0300

    Criando método ola que tal !

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ public class main{
     public static void main (String[] args){
         HelloWorld();
         OlaMundo();
+        Olaquetal();
+        
     }
     public static HelloWorld(){
         System.out.println("Hello world");
@@ -10,7 +12,9 @@ public class main{
     public static OlaMundo(){
         System.out.println("Olá mundo");
     }
-
+    public static Olaquetal(){
+        System.out.println("Olá quetal");
+    
 }
 
 
