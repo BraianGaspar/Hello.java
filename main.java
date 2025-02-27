8fd9a265ff6437aab887dbfd88d73a8e6c1a5da2
BRAIAN GASPAR DA ROSA <braian_gaspar@hotmail.com>
2025-02-26 21:42:47 -0300

    Criado método Olá Mundo!

--- main.java
+++ main.java
@@ -1,11 +1,16 @@
 public class main{
     public static void main (String[] args){
         HelloWorld();
+        OlaMundo();
     }
     public static HelloWorld(){
         System.out.println("Hello world");
     }
-    
+
+    public static OlaMundo(){
+        System.out.println("Olá mundo");
+    }
+
 }
 
 
